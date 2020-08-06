d3584e3e446bf99bdea6c5e4a05970c10a6ae880
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-05 22:33:56 -0700

    added get routes for about me, employments, skills, projects, extracurriculars, and resume pages

--- routes/html.js
+++ routes/html.js
@@ -1,6 +1,30 @@
 var db = require("../models");
 module.exports = function (app) {
-    app.get("/", function (req, res) {
+    app.get("/add", function (req, res) {
         res.render("add")
-    })
+    });
+
+    app.get("/", function (req, res) {
+        res.render("home")
+    });
+
+    app.get("/skills", function (req, res) {
+        res.render("skills")
+    });
+
+    app.get("/employments", function (req, res) {
+        res.render("employments")
+    });
+
+    app.get("/extracurriculars", function (req, res) {
+        res.render("extracurriculars")
+    });
+
+    app.get("/projects", function (req, res) {
+        res.render("projects")
+    });
+
+    app.get("/resume", function (req, res) {
+        res.render("resume")
+    });
 }
